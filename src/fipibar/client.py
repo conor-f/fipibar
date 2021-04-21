fd2cd276a678e69487baabf363025f8c29d3bb60
Conor Flynn <conor-f@users.noreply.github.com>
2021-04-21 23:36:17 +0100

    Adds change station methods (#2)

--- src/fipibar/client.py
+++ src/fipibar/client.py
@@ -19,10 +19,8 @@ class FipibarClient():
         self.config = FipibarConfig()
         self.unique_fip_process_string = 'fipibar_magic_constant'
 
-        # Thanks to Zopieux for the gist this came from:
-        # https://gist.github.com/Zopieux/ccb8d29437765083e4c80da52f2145b2
-        # URL for Fip Groove.
-        self.details_url = 'https://api.radiofrance.fr/livemeta/pull/66'
+        self.stations_list = self.get_stations_list()
+        self.current_station = self.config.get('current_station', 0)
 
         self.currently_playing_trunclen = int(
             self.config.get('currently_playing_trunclen', 45)
@@ -48,6 +46,53 @@ class FipibarClient():
                 print("Please configure ~/.fipibar_config.json with last.fm details.")
                 print(e)
 
+    def get_stations_list(self):
+        '''
+        Returns a list of dicts definining stations.
+        '''
+        return [
+            {
+                'name': 'FIP Groove',
+                'details_url': 'https://api.radiofrance.fr/livemeta/pull/66',
+                'stream_url': 'https://stream.radiofrance.fr/fipgroove/fipgroove_hifi.m3u8\?id\=radiofrance'
+            },
+            {
+                'name': 'FIP Radio',
+                'details_url': 'https://api.radiofrance.fr/livemeta/pull/7',
+                'stream_url': 'https://stream.radiofrance.fr/fip/fip_hifi.m3u8\?id\=radiofrance'
+            },
+            {
+                'name': 'FIP Rock',
+                'details_url': 'https://api.radiofrance.fr/livemeta/pull/64',
+                'stream_url': 'https://stream.radiofrance.fr/fiprock/fiprock_hifi.m3u8\?id\=radiofrance'
+            },
+            {
+                'name': 'FIP Jazz',
+                'details_url': 'https://api.radiofrance.fr/livemeta/pull/65',
+                'stream_url': 'https://stream.radiofrance.fr/fipjazz/fipjazz_hifi.m3u8\?id\=radiofrance'
+            },
+            {
+                'name': 'FIP World',
+                'details_url': 'https://api.radiofrance.fr/livemeta/pull/69',
+                'stream_url': 'https://stream.radiofrance.fr/fipworld/fipworld_hifi.m3u8\?id\=radiofrance'
+            },
+            {
+                'name': 'FIP Nouveautes',
+                'details_url': 'https://api.radiofrance.fr/livemeta/pull/70',
+                'stream_url': 'https://stream.radiofrance.fr/fipnouveautes/fipnouveautes_hifi.m3u8\?id\=radiofrance'
+            },
+            {
+                'name': 'FIP Reggae',
+                'details_url': 'https://api.radiofrance.fr/livemeta/pull/71',
+                'stream_url': 'https://stream.radiofrance.fr/fipreggae/fipreggae_hifi.m3u8\?id\=radiofrance'
+            },
+            {
+                'name': 'FIP Electro',
+                'details_url': 'https://api.radiofrance.fr/livemeta/pull/74',
+                'stream_url': 'https://stream.radiofrance.fr/fipelectro/fipelectro_hifi.m3u8\?id\=radiofrance'
+            },
+        ]
+
     def is_currently_playing(self):
         '''
         Returns True if there is a currently playing song, False otherwise.
@@ -58,12 +103,10 @@ class FipibarClient():
             return False
 
     def play(self):
-        if not self.is_currently_playing():
-            subprocess.check_output('bash -c "exec -a ' + self.unique_fip_process_string + ' ffplay -nodisp https://stream.radiofrance.fr/fipgroove/fipgroove_hifi.m3u8\?id\=radiofrance > /dev/null 2>&1 &"', shell=True)
+        subprocess.check_output('bash -c "exec -a ' + self.unique_fip_process_string + ' ffplay -nodisp ' + self.stations_list[self.config.get('current_station', 0)]['stream_url'] + ' > /dev/null 2>&1 &"', shell=True)
 
     def pause(self):
-        if self.is_currently_playing():
-            subprocess.check_output('kill $(ps aux | grep ' + self.unique_fip_process_string + ' | grep -v grep | awk -e \'{printf $2}\')', shell=True)
+        subprocess.check_output('kill $(ps aux | grep ' + self.unique_fip_process_string + ' | grep -v grep | awk -e \'{printf $2}\')', shell=True)
 
     def toggle_playback(self):
         '''
@@ -75,13 +118,21 @@ class FipibarClient():
         else:
             self.play()
 
+    def get_current_station_name(self):
+        return self.stations_list[self.current_station]['name']
+
+    def print_current_station_name(self):
+        print(self.get_current_station_name())
+
     def get_currently_playing_string(self):
         '''
         Returns the string displayed in Polybar for currently playing
         track/artist.
         '''
         try:
-            data = requests.get(self.details_url).json()
+            # Thanks to Zopieux for the gist this came from:
+            # https://gist.github.com/Zopieux/ccb8d29437765083e4c80da52f2145b2
+            data = requests.get(self.stations_list[self.config.get('current_station', 0)]['details_url']).json()
 
             level = data['levels'][0]
             uid = level['items'][level['position']]
@@ -96,7 +147,7 @@ class FipibarClient():
                             'notify-send',
                             '-i',
                             'applications-multimedia',
-                            'FIP radio',
+                            self.get_current_station_name(),
                             currently_playing_string
                         ]
                     )
@@ -124,6 +175,28 @@ class FipibarClient():
         except:
             return 'Can\'t find details...'
 
+    def next_station(self):
+        '''
+        Scroll to the next station in the list looping.
+        '''
+        self.current_station += 1
+        self.current_station %= len(self.stations_list)
+        self.config.set('current_station', self.current_station)
+
+        self.pause()
+        self.play()
+
+    def previous_station(self):
+        '''
+        Scroll to the previous station in the list looping.
+        '''
+        self.current_station -= 1
+        self.current_station %= len(self.stations_list)
+        self.config.set('current_station', self.current_station)
+
+        self.pause()
+        self.play()
+
 
 def main():
     fipibar_client = FipibarClient()
@@ -135,6 +208,9 @@ def main():
     group = parser.add_mutually_exclusive_group()
     group.add_argument("--get-currently-playing", action="store_true")
     group.add_argument("--toggle-playback", action="store_true")
+    group.add_argument("--next-station", action="store_true")
+    group.add_argument("--previous-station", action="store_true")
+    group.add_argument("--print-current-station-name", action="store_true")
 
     args = parser.parse_args()
 
@@ -142,6 +218,12 @@ def main():
         print(fipibar_client.get_currently_playing_string())
     elif args.toggle_playback:
         fipibar_client.toggle_playback()
+    elif args.next_station:
+        fipibar_client.next_station()
+    elif args.previous_station:
+        fipibar_client.previous_station()
+    elif args.print_current_station_name:
+        fipibar_client.print_current_station_name()
 
 
 if __name__ == '__main__':
