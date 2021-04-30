6a19003be872ed11de80ceaf9087be0688f66115
Conor Flynn <conor@glimpse.ie>
2021-04-30 01:06:18 +0100

    Bumps to 0.0.6.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ INSTALL_REQUIRES = [
 setup(
     name='fipibar',
     description='Fip radio plugin for Polybar',
-    version='0.0.5',
+    version='0.0.6',
     url='https://github.com/conor-f/fipibar',
     python_requires='>=3.6',
     packages=find_packages('src'),
